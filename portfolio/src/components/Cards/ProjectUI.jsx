ec7defcc5eab600bae0f1f1711731313a35ce8d9
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-11-30 00:10:14 +0530

    add project showcase

--- portfolio/src/components/Cards/ProjectUI.jsx
+++ portfolio/src/components/Cards/ProjectUI.jsx
@@ -1,102 +1,112 @@
 "use client";
 import React from "react";
 import { HeroParallax } from "../ui/hero-parallax";
+import Myntra from "../../assets/projectThumbnail/myntra.png";
+import Myntra2 from "../../assets/projectThumbnail/myntra2.png";
+import ScenicWeb from "../../assets/projectThumbnail/scenicWeb.png";
+import ScenicWeb2 from "../../assets/projectThumbnail/scenicWeb2.png";
+import Calculetor from "../../assets/projectThumbnail/calculetor.png";
+import KeyStrike from "../../assets/projectThumbnail/KeyStrike.png";
+import ShopiVibe from "../../assets/projectThumbnail/ShopiVibe.png";
 
 export function ProjectUI() {
   return <HeroParallax products={products} />;
 }
 export const products = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+    title: "Myntra",
+    link: "https://cool-lamingto-edc51b.netlify.app/",
+    thumbnail: Myntra,
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+    title: "Myntra ",
+    link: "https://cool-lamingto-edc51b.netlify.app/",
+    thumbnail: Myntra2,
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+    title: "Scenic Web",
+    link: "https://prismatic-gingersnap-2bdf3f.netlify.app",
+    thumbnail: ScenicWeb,
   },
 
   {
-    title: "Editorially",
-    link: "https://editorially.org",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+    title: "Scenic Web ",
+    link: "https://prismatic-gingersnap-2bdf3f.netlify.app",
+    thumbnail: ScenicWeb2,
   },
   {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+    title: "Calculator App",
+    link: "https://calculetor-index-002.netlify.app",
+    thumbnail: Calculetor,
   },
   {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+    title: "Key Strike",
+    link: "https://typing-speed-shubhamsingh0007.netlify.app",
+    thumbnail: KeyStrike,
   },
 
   {
-    title: "Algochurn",
-    link: "https://algochurn.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+    title: "ShopiVibe",
+    link: "#",
+    thumbnail: ShopiVibe,
   },
   {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+    title: "Calculator App",
+    link: "https://calculetor-index-002.netlify.app",
+    thumbnail: Calculetor,
   },
   {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    title: "Key Strike",
+    link: "https://typing-speed-shubhamsingh0007.netlify.app",
+    thumbnail: KeyStrike,
   },
-  {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-  },
-  {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  },
+  // {
+  //   title: "Aceternity UI",
+  //   link: "https://ui.aceternity.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  // },
+  // {
+  //   title: "Tailwind Master Kit",
+  //   link: "https://tailwindmasterkit.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  // },
+  // {
+  //   title: "SmartBridge",
+  //   link: "https://smartbridgetech.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  // },
+  // {
+  //   title: "Renderwork Studio",
+  //   link: "https://renderwork.studio",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  // },
 
-  {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-  },
-  {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-  },
-  {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-  },
-  {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-  },
+  // {
+  //   title: "Creme Digital",
+  //   link: "https://cremedigital.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  // },
+  // {
+  //   title: "Golden Bells Academy",
+  //   link: "https://goldenbellsacademy.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  // },
+  // {
+  //   title: "Invoker Labs",
+  //   link: "https://invoker.lol",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  // },
+  // {
+  //   title: "E Free Invoice",
+  //   link: "https://efreeinvoice.com",
+  //   thumbnail:
+  //     "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  // },
 ];
--- portfolio/src/components/ui/hero-parallax.jsx
+++ portfolio/src/components/ui/hero-parallax.jsx
@@ -87,7 +87,7 @@ export const HeroParallax = ({ products }) => {
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40  w-full  left-0 top-0">
       <h1 className="text-4xl md:text-7xl font-bold dark:text-white">
         Project Showcase
       </h1>
